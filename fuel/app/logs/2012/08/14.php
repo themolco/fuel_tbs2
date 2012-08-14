6078a9eebd047b5b7a21b3f975f5277e574a8798
Michael Holcombe <the.molco@gmail.com>
2012-08-13 22:50:09 -0700

    bootstrap 2.0 added. working on template

--- fuel/app/logs/2012/08/14.php
+++ fuel/app/logs/2012/08/14.php
@@ -5,3 +5,4 @@ Warning - 2012-08-14 05:13:50 --> Fuel\Core\Fuel::init - The configured locale e
 Warning - 2012-08-14 05:15:00 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
 Warning - 2012-08-14 05:15:19 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
 Warning - 2012-08-14 05:15:58 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+Warning - 2012-08-14 05:49:00 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
