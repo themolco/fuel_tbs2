08442efb7ff0ae47fe74fd0e9ace262c36bd7fbc
Michael Holcombe <the.molco@gmail.com>
2012-08-13 22:14:37 -0700

    minor tweak

---
+++ fuel/app/logs/2012/08/14.php
@@ -0,0 +1,4 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+Warning - 2012-08-14 04:40:27 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+Warning - 2012-08-14 05:13:50 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
