af93091b105014c5972b8b0e2238cb6c6b5171c0
Michael Holcombe <the.molco@gmail.com>
2012-08-13 22:16:10 -0700

    bootstrap ignored

--- fuel/app/logs/2012/08/14.php
+++ fuel/app/logs/2012/08/14.php
@@ -2,3 +2,6 @@
 
 Warning - 2012-08-14 04:40:27 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
 Warning - 2012-08-14 05:13:50 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+Warning - 2012-08-14 05:15:00 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+Warning - 2012-08-14 05:15:19 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+Warning - 2012-08-14 05:15:58 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
